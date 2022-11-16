1183c42fb0dc326de89b33f13a5eafb31bd0220d
MCSeal <mzeal93@gmail.com>
2022-11-16 14:38:52 -0500

    workedo nhtml

--- public/wp-content/themes/wp-devs/index.php
+++ public/wp-content/themes/wp-devs/index.php
@@ -1,15 +1,42 @@
 <!DOCTYPE html>
-<html lang="en">
+<html <?php language_attributes(); ?>>
 
 <head>
-    <meta charset="UTF-8">
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+
 </head>
 
-<body>
+<body <?php body_class(); ?>>
+    <div id="page" class="site">
+        <header>
+            <section class="top-bar">
+                <div class="logo">
+                    <div class="searchbox"></div>
+                </div>
+            </section>
 
+            <section class="menu-area">
+
+            </section>
+        </header>
+        <div id="content">
+            <div id="primary">
+                <main id="main">
+                    <section class="hero">
+                        <section class="services">
+                            <section class="home-blog"></section>
+
+                        </section>
+                    </section>
+                </main>
+            </div>
+        </div>
+        <footer class="site-footer">
+
+        </footer>
+    </div>
 </body>
 
 </html>
