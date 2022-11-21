993a76a460c5551fea3c16973b7f5a74d8cf724d
MCSeal <mzeal93@gmail.com>
2022-11-21 16:06:50 -0500

    added font

--- public/wp-content/themes/wp-devs/header.php
+++ public/wp-content/themes/wp-devs/header.php
@@ -5,6 +5,7 @@
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php wp_head(); ?>
 
 </head>
 
@@ -14,7 +15,7 @@
             <section class="top-bar">
                 <div class="logo">
                     <div class="searchbox"></div>
-                    search
+                    Search
                 </div>
             </section>
 
